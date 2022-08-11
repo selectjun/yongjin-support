96a9c68d029052f9d6e20bd42613afa1c2cf8963
selectjun <selectjun@gmail.com>
2022-08-11 23:32:19 +0900

    Database 설정.

--- yongjin-support-server/src/app.module.ts
+++ yongjin-support-server/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BoardModule } from './board/board.module';
+import { validate } from './env.validation';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BoardModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
