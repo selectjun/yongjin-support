d8637f328584cad0f47d177a3bb32cd01448807c
selectjun <selectjun@gmail.com>
2022-08-11 20:57:04 +0900

    인증, 사용자, 게시판 모듈 추가.

--- yongjin-support-server/src/app.module.ts
+++ yongjin-support-server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
